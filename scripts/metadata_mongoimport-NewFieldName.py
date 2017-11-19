8ecbdc0d02106e8b7ec3fd92257054545bc163b9
Richard Bruskiewich <richard@starinformatics.com>
2017-11-18 21:51:40 -0800

    small typo

--- scripts/metadata_mongoimport-NewFieldName.py
+++ scripts/metadata_mongoimport-NewFieldName.py
@@ -78,7 +78,7 @@ def process():
 
 	if not options.inputFileName: return
 	
-	df = pd.read_csv( inputFileName, sep=None )"
+	df = pd.read_csv( inputFileName, sep=None )
 	
 	# Yang: there is an extra field with the same name library_source
 	# if bojan delete that field, I need to change this code
