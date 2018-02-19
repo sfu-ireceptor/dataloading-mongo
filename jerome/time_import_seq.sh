39a3c46379faae78fb590b6c65d1e55a00c35e0f
Jerome Jaglale <jerome_jaglale@sfu.ca>
2018-02-19 13:43:11 -0800

    Fix typo

--- jerome/time_import_seq.sh
+++ jerome/time_import_seq.sh
@@ -5,5 +5,5 @@ cd /home/ubuntu/test/scratch
 for i in `seq 100`
 do
 	python /home/ubuntu/test/dataloading-mongo/yang_script/sequenceLoading_imgt_20180125.py db1 sequences samples /home/ubuntu/test/data_to_load/seq/
-	du -skh du -skh /mnt/mongodbdata
+	du -skh /mnt/mongodbdata
 done
