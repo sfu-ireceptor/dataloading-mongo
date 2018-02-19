823064802461f3c97283ee34c3f7844f1f767501
Jerome Jaglale <jerome_jaglale@sfu.ca>
2018-02-19 13:34:26 -0800

    Update path to mongo folder

--- jerome/time_import_seq.sh
+++ jerome/time_import_seq.sh
@@ -5,5 +5,5 @@ cd /home/ubuntu/test/scratch
 for i in `seq 100`
 do
 	python /home/ubuntu/test/dataloading-mongo/yang_script/sequenceLoading_imgt_20180125.py db1 sequences samples /home/ubuntu/test/data_to_load/seq/
-	du -skh /data/db
+	du -skh du -skh /mnt/mongodbdata
 done
