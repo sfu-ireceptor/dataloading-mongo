17bf28af1e82ced061bb468570e8b9f4e63ef9f6
Jerome Jaglale <jerome_jaglale@sfu.ca>
2018-02-20 12:27:09 -0800

    Change file paths

--- jerome/time_import_seq.sh
+++ jerome/time_import_seq.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-cd /home/ubuntu/test/scratch
+cd /home/ubuntu/scratch
 
 for i in `seq 100`
 do
-	python /home/ubuntu/test/dataloading-mongo/yang_script/sequenceLoading_imgt_20180125.py db1 sequences samples /home/ubuntu/test/data_to_load/seq/
+	python /home/ubuntu/dataloading-mongo/yang_script/sequenceLoading_imgt_20180125.py db1 sequences samples /home/ubuntu/data_to_load/seq/
 	du -skh /mnt/mongodbdata
 done
