f29618e02a91162312ba60e58fb13f6250bb7a93
Jerome Jaglale <jerome_jaglale@sfu.ca>
2018-04-13 14:04:37 -0700

    Simplify code

--- jerome/test_performance.js
+++ jerome/test_performance.js
@@ -1,60 +1,66 @@
+// MongoDB collection
 var collection = 'sequences';
 
+// filter values
+var filter_v_call = '^TRBV20-1\\*01';
+var filter_j_call = '^TRBJ1-5\\*02';
+var filter_d_call = '^TRBD2\\*01';
+var filter_substring = 'CASSQVGTGVY';
+vat filter_junction_aa_length = 6;
+
+// init result arrays
 var equals = [];
 var equals_res = [];
-
 var substring = [];
 var substring_res = [];
-
 var vregex = [];
 var vregex_res = [];
-
 var dregex = [];
 var dregex_res = [];
-
 var jregex = [];
 var jregex_res = [];
-
 var total = [];
 var total_res = [];
-
 var range = [];
 var range_res = [];
 
+// get samples ids from *sequences* collection
 var sample_id_list = db[collection].distinct('ir_project_sample_id');
+
+print("sample id\tequal time\tequal results\tsubstring time\tsubstring results\tvregex time\tvregex results\tjregex time\tjregex results\tdregex time\tdregex results\ttotal time\ttotal results");
 sample_id_list.forEach(function(sample_id, i) {
        print('Doing queries for sample_id=' + sample_id);
 
        var t0 = new Date();
-       var sequence_count = db[collection].count({"ir_project_sample_id": sample_id, "v_call": {"$regex": '^TRBV20-1\\*01'}});
+       var sequence_count = db[collection].count({"ir_project_sample_id": sample_id, "v_call": {"$regex": filter_v_call}});
        var t1 = new Date();
        var duration = (t1  - t0)/1000;
        vregex[i] = duration;
        vregex_res[i] = sequence_count;
 
        var t0 = new Date();
-       var sequence_count = db[collection].count({"ir_project_sample_id": sample_id, "j_call": {"$regex": '^TRBJ1-5\\*02'}});
+       var sequence_count = db[collection].count({"ir_project_sample_id": sample_id, "j_call": {"$regex": filter_j_call}});
        var t1 = new Date();
        var duration = (t1  - t0)/1000;
        jregex[i] = duration;
        jregex_res[i] = sequence_count;
 
        var t0 = new Date();
-       var sequence_count = db[collection].count({"ir_project_sample_id": sample_id, "d_call": {"$regex": '^TRBD2\\*01'}});
+       var sequence_count = db[collection].count({"ir_project_sample_id": sample_id, "d_call": {"$regex": filter_d_call}});
        var t1 = new Date();
        var duration = (t1  - t0)/1000;
        dregex[i] = duration;
        dregex_res[i] = sequence_count;
 
        t0 = new Date();
-       sequence_count = db[collection].count({"ir_project_sample_id": sample_id, "substring": "CASSQVGTGVY"});
+       sequence_count = db[collection].count({"ir_project_sample_id": sample_id, "substring": filter_substring});
        t1 = new Date();
        duration = (t1  - t0)/1000;
        substring[i] = duration;
        substring_res[i] = sequence_count;
 
        t0 = new Date();
-       sequence_count = db[collection].count({"ir_project_sample_id": sample_id, "junction_aa_length": 6});
+       sequence_count = db[collection].count({"ir_project_sample_id": sample_id, "junction_aa_length": filter_junction_aa_length});
        t1 = new Date();
        duration = (t1  - t0)/1000;
        equals[i] = duration;
@@ -66,9 +72,6 @@ sample_id_list.forEach(function(sample_id, i) {
        duration = (t1  - t0)/1000;
        total[i] = duration;
        total_res[i] = sequence_count;
-});
 
-print("sample id\tequal time\tequal results\tsubstring time\tsubstring results\tvregex time\tvregex results\tjregex time\tjregex results\tdregex time\tdregex results\ttotal time\ttotal results");
-sample_id_list.forEach(function(sample_id, i) {
        print (i+ "\t" + equals[i] + "\t" + equals_res[i] + "\t" + substring[i] + "\t" + substring_res[i] + "\t" +vregex[i] + "\t" + vregex_res[i] + "\t" +jregex[i] + "\t" + jregex_res[i]+ "\t" +dregex[i] + "\t" + dregex_res[i] + "\t" + total[i] + "\t" + total_res[i]);
 });
