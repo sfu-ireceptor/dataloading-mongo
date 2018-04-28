9af2bf0a8970e84a81ac502e5f64c78c45a0e2f6
Brian Corrie <bdcorrie@gmail.com>
2018-04-28 00:35:06 +0000

    Changed it so use command line arguements to specify db and port

--- jerome/test-explain.sh
+++ jerome/test-explain.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
 count=5
-if [ $# -eq 1 ]
+if [ $# -eq 3 ]
 then
     count=$1
+    db_name=$2
+    db_port=$3
+else
+    echo "usage: $0 count db port"
+    exit
 fi
 
 js_file="test_performance_explain.js"
 host_name=$(hostname)
-db_name="db1"
 
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 echo "Test performed at: $current_time"
-mongo $db_name cache-dump.js > $host_name-$db_name-cache-$current_time.txt
+mongo $db_name --port $db_port cache-dump.js > $host_name-$db_name-cache-$current_time.txt
 
 for i in `seq 1 $count`;
 do
     echo "performing test iteration $i"
-    mongo $db_name $js_file > run$i-$host_name-$db_name-$current_time.txt
+    mongo $db_name --port $db_port $js_file > run$i-$host_name-$db_name-$current_time.txt
 done
 
