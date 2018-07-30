04d8c74fd2ced1d63d5bad1f6755e35919fd6e19
Jerome <jerome_jaglale@sfu.ca>
2018-07-30 21:49:24 +0000

    Fix bugs

--- util/add_sequence.py
+++ util/add_sequence.py
@@ -12,6 +12,7 @@ import os
 import sys
 from os import listdir
 from os.path import isfile, join
+import argparse
 
 import tarfile
 import zipfile
@@ -40,9 +41,9 @@ def load_file(file_path, collection):
 #        print("Unknown file")
 #        return();
 
-    print 'Processing file: ' + file_path
+    print('Processing file: ' + file_path)
     counter = 0
-    tempfile = mypath + 'temp.fasta'
+    tempfile = '/tmp/temp.fasta'
     with gzip.open(file_path) as f:
         with open(tempfile, 'wb') as out:
             shutil.copyfileobj(f, out)
@@ -55,14 +56,14 @@ def load_file(file_path, collection):
                     {'$set': {'sequence': sequence}})
             if update_query['nModified'] == 0:
                 update_query = \
-                    sequence_collection.update({'seq_name': imgt_header},
+                    collection.update({'seq_name': imgt_header},
                         {'$set': {'sequence': sequence}})
                 if update_query['nModified'] == 0:
-                    print 'Header + ' + header + ' converted to ' \
-                        + imgt_header + ' not found!'
+                    print ('Header + ' + header + ' converted to ' \
+                        + imgt_header + ' not found!')
             counter += 1
             if counter % 200000 == 0:
-                print 'Processed ' + str(counter) + ' lines'
+                print('Processed ' + str(counter) + ' lines')
 
     os.remove(tempfile)
 
@@ -85,13 +86,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('database', help='database name')
     parser.add_argument('collection', help='collection name')
-    parser.add_argument('fasta', help='fasta file name')
+    parser.add_argument('folder', help='folder of fasta files')
 
     # get arguments
     args = parser.parse_args()
    
     database = args.database
     collection = args.collection
-    files_folder = args.files_folder
+    folder = args.folder
 
-    main(database, collection, files_folder)
+    main(database, collection, folder)
