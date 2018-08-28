41e30ae3f092f5129cefd85416393d6f47c5ada0
Colin Qiao <colinq@gmail.com>
2018-08-27 19:27:52 -0700

    fix explain script for turnkey

--- testing/test_explain_turnkey.sh
+++ testing/test_explain_turnkey.sh
@@ -14,7 +14,6 @@ else
     exit
 fi
 
-TEST="dbtest"
 OUT="out"
 perf_js_file="test_performance_explain.js"
 cache_js_file="cache_dump.js"
@@ -28,12 +27,12 @@ echo "Test performed at: $current_time"
 
 mkdir -p $RECENT
 
-sudo docker exec -it irdn-mongo mongo --authenticationDatabase admin $db_name -u $MONGODB_SERVICE_USER -p $MONGODB_SERVICE_SECRET $TEST/$cache_js_file > $RECENT/cache-$host_name-$db_port-$db_name-$current_time.txt
+sudo docker exec -it irdn-mongo mongo --authenticationDatabase admin $db_name -u $MONGODB_SERVICE_USER -p $MONGODB_SERVICE_SECRET ./$cache_js_file > $RECENT/cache-$host_name-$db_port-$db_name-$current_time.txt
 
 for i in `seq 1 $count`;
 do
     echo "Performing test iteration $i"
-    sudo docker exec -it irdn-mongo mongo --authenticationDatabase admin $db_name -u $MONGODB_SERVICE_USER -p $MONGODB_SERVICE_SECRET $TEST/$perf_js_file > $RECENT/run$i-$host_name-$db_port-$db_name-$current_time.txt
+    sudo docker exec -it irdn-mongo mongo --authenticationDatabase admin $db_name -u $MONGODB_SERVICE_USER -p $MONGODB_SERVICE_SECRET ./$perf_js_file > $RECENT/run$i-$host_name-$db_port-$db_name-$current_time.txt
 
 done
 end_time=$(date "+%Y.%m.%d-%H.%M.%S")
