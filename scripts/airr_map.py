2fdec3579163235a886b5a404d98c970392be015
Brian Corrie <bdcorrie@gmail.com>
2018-10-26 22:11:29 +0000

    Did some clean up on the mapping, made file loading and class instance creation independent. Renamed some variables...

--- scripts/airr_map.py
+++ scripts/airr_map.py
@@ -3,28 +3,44 @@
 import pandas as pd
 
 class AIRRMap:
-    def __init__(self, mapfile):
+    def __init__(self, verbose):
         # Set up initial class mappings from the file. These are defined in the MiAIRR Standard.
-        self.airr_metadata_classes = ["study", "subject", "diagnosis", "sample",
-                                      "cell_processing", "nucleic_acid_processing", "sequencing_run", "software_processing"]
+        # Repertoire metadata spans a number of MiAIRR data class types.
+        self.airr_repertoire_classes = ["study", "subject", "diagnosis", "sample",
+                                        "cell_processing", "nucleic_acid_processing",
+                                        "sequencing_run", "software_processing"]
+        # Rearrangement data has only one class in the MiAIRR standard.
         self.airr_rearrangement_classes = ["rearrangement"]
         # Keep track of the mapfile being used.
-        self.mapfile = mapfile
-        # Read in the mapfile
-        self.readMapFile(self.mapfile)
-
+        self.mapfile = ""
+        # Keep track of the verbosity flag.
+        self.verbose = verbose
+        # Initialize the internal data structures
+        self.airr_mappings = []
+        self.airr_rearrangement_map = []
+        self.airr_repertiore_map = []
+        
     # Read in a map file given a file name.
     def readMapFile(self, mapfile):
-        self.mapfile = mapfile
+        # Load the mapfile in.
         try:
-            self.airr_mappings = pd.read_csv(self.mapfile, sep='\t')
+            self.airr_mappings = pd.read_csv(mapfile, sep='\t')
         except:
-            print("Error: Could not load AIRR Map file %s" % self.mapfile)
+            print("Error: Could not load AIRR Map file %s" % mapfile)
             return False 
+
+        # If we have read a mapfile, keep track of the file name.
+        self.mapfile = mapfile
+
+        # Write some diagnostics about the file read in
+        if self.verbose:
+            print("Successfully read in %d mapping columns" % (len(self.airr_mappings.columns)))
+
+        # Get the rows that we care about for MiAIRR repertiores (sample loading)
+        # and MiAIRR rearrangments.
         self.airr_rearrangement_map = self.airr_mappings.loc[self.airr_mappings['airr_class'].isin(self.airr_rearrangement_classes)]
-        self.airr_metadata_map = self.airr_mappings.loc[self.airr_mappings['airr_class'].isin(self.airr_metadata_classes)]
-        #print(self.airr_rearrangement_map)
-        #print(self.airr_metadata_map)
+        self.airr_repertoire_map = self.airr_mappings.loc[self.airr_mappings['airr_class'].isin(self.airr_repertoire_classes)]
+        #print(self.airr_repertoire_map['ir_id'])
         return True
 
 
