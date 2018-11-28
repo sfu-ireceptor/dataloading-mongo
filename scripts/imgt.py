7e00f9bccb4bfe04c1cee7ab3d43ad966982cb27
Brian Corrie <bdcorrie@gmail.com>
2018-11-28 20:29:51 +0000

    Added some debugging statements about the mapping.
    Did a careful check to make sure that all of the fields that were in the original
    mapping are in the AIRR_Mapping file. Annotated each field in the code and commented
    it out as it is no longer needed because of the mapping.
    
    Tested the code on the simple example file, seems to work. More testing required.

--- scripts/imgt.py
+++ scripts/imgt.py
@@ -89,8 +89,9 @@ class IMGT(Parser):
         # that we should be considering.
         vquest_file_map = self.context.airr_map.airr_rearrangement_map['vquest_file']
         vquest_files = vquest_file_map.dropna().unique()
-        print("VQuest Files")
-        print(vquest_files)
+        if self.context.verbose:
+            print("VQuest Files")
+            print(vquest_files)
         # Create a dictionary that stores an array of fields to process
         # for each IMGT file that we need to process.
         filedict = {}
@@ -107,6 +108,9 @@ class IMGT(Parser):
             # the specific formats (e.g. ir_id, airr, vquest). The rows are limited to have
             # only data that is relevant to this specific vquest file.
             file_fields = self.context.airr_map.airr_rearrangement_map.loc[field_of_interest]
+            if self.context.verbose:
+                for index, row in file_fields.iterrows():
+                    print("    " + row['vquest'] + " -> " + row['airr'])
             # Use the vquest column in our mapping to select the columns we want from the 
             # possibly quite large vquest data frame.
             airr_dataframe = vquest_dataframe[file_fields['vquest']]
@@ -128,163 +132,157 @@ class IMGT(Parser):
                 vquest_concat = pd.concat([vquest_concat, vquest_dataframe], axis=1)
                 
 
-        #print(filedict)
-        print(filedict.keys())
-        #print(mongo_concat)
-        #print(filedict.values())
-
-
-        Summary_1 = self.readDf('1_Summary.txt')
-
-        IMGT_gapped_nt_sequences_2 = self.readDf(
-            '2_IMGT-gapped-nt-sequences.txt')
-
-        Nt_sequences_3 = self.readDf('3_Nt-sequences.txt')
-
-        IMGT_gapped_AA_sequences_4 = self.readDf(
-            '4_IMGT-gapped-AA-sequences.txt')
-
-        AA_sequences_5 = self.readDf('5_AA-sequences.txt')
-
-        V_REGION_mutation_and_AA_change_table_7 = self.readDf(
-            '7_V-REGION-mutation-and-AA-change-table.txt')
+#        Summary_1 = self.readDf('1_Summary.txt')
+#
+#        IMGT_gapped_nt_sequences_2 = self.readDf(
+#            '2_IMGT-gapped-nt-sequences.txt')
+#
+#        Nt_sequences_3 = self.readDf('3_Nt-sequences.txt')
+#
+#        IMGT_gapped_AA_sequences_4 = self.readDf(
+#            '4_IMGT-gapped-AA-sequences.txt')
+#
+#        AA_sequences_5 = self.readDf('5_AA-sequences.txt')
+#
+#        V_REGION_mutation_and_AA_change_table_7 = self.readDf(
+#            '7_V-REGION-mutation-and-AA-change-table.txt')
 
         Parameters_11 = self.readDfNoHeader('11_Parameters.txt')
-        print(Parameters_11)
+        #print(Parameters_11)
 
         # Create a dictionary with keys the first column of the parameter file and the values
         # the second column in the parameter file.
         parameter_dictionary = dict(zip(Parameters_11[0], Parameters_11[1]))
-        print(parameter_dictionary)
+        #print(parameter_dictionary)
 
-        Summary_column_list = Summary_1.columns.values.tolist()
+#        Summary_column_list = Summary_1.columns.values.tolist()
 
-        if 'Functionality' in Summary_column_list:
+#        if 'Functionality' in Summary_column_list:
+#
+#            df_1 = Summary_1[[
+#                'XXSequence ID', 'XXV-GENE and allele', 'XXJ-GENE and allele',
+#                'XXD-GENE and allele', 'YYFunctionality', 'XXV-REGION score',
+#                'XXJ-REGION score', 'XXV-REGION identity %',
+#                'XXD-REGION reading frame', 'XXCDR1-IMGT length',
+#                'XXCDR2-IMGT length', 'XXCDR3-IMGT length',
+#                'YYFunctionality comment', 'XXOrientation', 'ERROR in OLD CODE - THIS should be J!!!V-REGION identity %'
+#            ]]
+#
+#            df_1.columns = [
+#                'seq_name', 'v_string', 'j_string', 'd_string', 'functionality',
+#                'v_score', 'j_score', 'vgene_probablity',
+#                'dregion_reading_frame', 'cdr1_length', 'cdr2_length',
+#                'cdr3_length', 'functionality_comment', 'rev_comp',
+#                'vgene_probability'
+#            ]
+#
+#        elif 'V-DOMAIN Functionality' in Summary_column_list:
+#
+#            df_1 = Summary_1[[
+#                'XXSequence ID', 'XXV-GENE and allele', 'XXJ-GENE and allele',
+#                'XXD-GENE and allele', 'XXV-DOMAIN Functionality',
+#                'XXV-REGION score', 'XXJ-REGION score', 'XXV-REGION identity %',
+#                'XXD-REGION reading frame', 'XXCDR1-IMGT length',
+#                'XXCDR2-IMGT length', 'XXCDR3-IMGT length',
+#                'XXV-DOMAIN Functionality comment', 'XXOrientation',
+#                'ERROR!!!V-REGION identity %'
+#            ]]
+#
+#            df_1.columns = [
+#                'seq_name', 'v_string', 'j_string', 'd_string', 'functionality',
+#                'v_score', 'j_score', 'vgene_probablity',
+#                'dregion_reading_frame', 'cdr1_length', 'cdr2_length',
+#                'cdr3_length', 'functionality_comment', 'rev_comp',
+#                'vgene_probability'
+#            ]
+#
+#        df_2 = IMGT_gapped_nt_sequences_2[[
+#            'XXV-D-J-REGION', 'XXV-J-REGION', 'XXV-REGION', 'XXJ-REGION', 'XXFR1-IMGT',
+#            'XXFR2-IMGT', 'XXFR3-IMGT', 'XXFR4-IMGT', 'XXCDR1-IMGT', 'XXCDR2-IMGT',
+#            'XXCDR3-IMGT', 'XXJUNCTION'
+#        ]]
+#
+#        df_2.columns = [
+#            'vdjregion_sequence_nt_gapped', 'vjregion_sequence_nt_gapped',
+#            'vregion_sequence_nt_gapped', 'jregion_sequence_nt_gapped',
+#            'fr1region_sequence_nt_gapped', 'fr2region_sequence_nt_gapped',
+#            'fr3region_sequence_nt_gapped', 'fr4region_sequence_nt_gapped',
+#            'cdr1region_sequence_nt_gapped', 'cdr2region_sequence_nt_gapped',
+#            'cdr3region_sequence_nt_gapped', 'junction_sequence_nt_gapped'
+#        ]
+#
+#        df_3 = Nt_sequences_3[[
+#            'XXV-D-J-REGION', 'XXV-J-REGION', 'XXD-J-REGION', 'XXV-REGION', 'XXJ-REGION',
+#            'XXD-REGION', 'XXFR1-IMGT', 'XXFR2-IMGT', 'XXFR3-IMGT', 'XXFR4-IMGT',
+#            'XXCDR1-IMGT', 'XXCDR2-IMGT', 'XXCDR3-IMGT', '???????????JUNCTION',
+#            'XXV-D-J-REGION start', 'XXV-D-J-REGION end', 'XXV-J-REGION start',
+#            'XXV-J-REGION end', 'XXV-REGION start', 'XXV-REGION end',
+#            'XXJ-REGION start', 'XXJ-REGION end', 'XXD-REGION start', 'XXD-REGION end',
+#            'XXFR1-IMGT start', 'XXFR1-IMGT end', 'XXFR2-IMGT start', 'XXFR2-IMGT end',
+#            'XXFR3-IMGT start', 'XXFR3-IMGT end', 'XXFR4-IMGT start', 'XXFR4-IMGT end',
+#            'XXCDR1-IMGT start', 'XXCDR1-IMGT end', 'XXCDR2-IMGT start',
+#            'XXCDR2-IMGT end', 'XXCDR3-IMGT start', 'XXCDR3-IMGT end',
+#            'XXJUNCTION start', 'XXJUNCTION end', 'XXD-J-REGION start',
+#            'XXD-J-REGION end'
+#        ]]
+#
+#        df_3.columns = [
+#            'vdjregion_sequence_nt', 'vjregion_sequence_nt',
+#            'djregion_sequence_nt', 'vregion_sequence_nt',
+#            'jregion_sequence_nt', 'dregion_sequence_nt',
+#            'fr1region_sequence_nt', 'fr2region_sequence_nt',
+#            'fr3region_sequence_nt', 'fr4region_sequence_nt',
+#            'cdr1region_sequence_nt', 'cdr2region_sequence_nt',
+#            'cdr3region_sequence_nt', 'junction_nt', 'vdjregion_start',
+#            'vdjregion_end', 'vjregion_start', 'vjregion_end', 'v_start',
+#            'v_end', 'j_start', 'j_end', 'd_start', 'd_end', 'fwr1_start',
+#            'fwr1_end', 'fwr2_start', 'fwr2_end', 'fwr3_start', 'fwr3_end',
+#            'fwr4_start', 'fwr4_end', 'cdr1_start', 'cdr1_end', 'cdr2_start',
+#            'cdr2_end', 'cdr3_start', 'cdr3_end', 'junction_start',
+#            'junction_end', 'djregion_start', 'djregion_end'
+#        ]
+#
+#        df_4 = IMGT_gapped_AA_sequences_4[[
+#            'XXV-D-J-REGION', 'XXV-J-REGION', 'XXV-REGION', 'XXJ-REGION', 'XXFR1-IMGT',
+#            'XXFR2-IMGT', 'XXFR3-IMGT', 'XXFR4-IMGT', 'XXCDR1-IMGT', 'XXCDR2-IMGT',
+#            'XXCDR3-IMGT', 'XXJUNCTION'
+#        ]]
+#
+#        df_4.columns = [
+#            'vdjregion_sequence_aa_gapped', 'vjregion_sequence_aa_gapped',
+#            'vregion_sequence_aa_gapped', 'jregion_sequence_aa_gapped',
+#            'fr1region_sequence_aa_gapped', 'fr2region_sequence_aa_gapped',
+#            'fr3region_sequence_aa_gapped', 'fr4region_sequence_aa_gapped',
+#            'cdr1region_sequence_aa_gapped', 'cdr2region_sequence_aa_gapped',
+#            'cdr3region_sequence_aa_gapped', 'junction_sequence_aa_gapped'
+#        ]
+#
+#        df_5 = AA_sequences_5[[
+#            'XXV-D-J-REGION', 'XXV-J-REGION', 'XXV-REGION', 'XXJ-REGION', 'XXFR1-IMGT',
+#            'XXFR2-IMGT', 'XXFR3-IMGT', 'XXFR4-IMGT', 'XXCDR1-IMGT', 'XXCDR2-IMGT',
+#            'XXCDR3-IMGT', 'XXJUNCTION'
+#        ]]
+#
+#        df_5.columns = [
+#            'vdjregion_sequence_aa', 'vjregion_sequence_aa',
+#            'vregion_sequence_aa', 'jregion_sequence_aa',
+#            'fr1region_sequence_aa', 'fr2region_sequence_aa',
+#            'fr3region_sequence_aa', 'fr4region_sequence_aa',
+#            'cdr1region_sequence_aa', 'cdr2region_sequence_aa',
+#            'cdr3region_sequence_aa', 'junction_aa'
+#        ]
+#
+        #df_7 = V_REGION_mutation_and_AA_change_table_7[[
+        #    'XXV-REGION', 'XXFR1-IMGT', 'XXFR2-IMGT', 'XXFR3-IMGT', 'XXCDR1-IMGT',
+        #    'XXCDR2-IMGT', 'XXCDR3-IMGT'
+        #]]
 
-            df_1 = Summary_1[[
-                'Sequence ID', 'V-GENE and allele', 'J-GENE and allele',
-                'D-GENE and allele', 'Functionality', 'V-REGION score',
-                'J-REGION score', 'V-REGION identity %',
-                'D-REGION reading frame', 'CDR1-IMGT length',
-                'CDR2-IMGT length', 'CDR3-IMGT length',
-                'Functionality comment', 'Orientation', 'V-REGION identity %'
-            ]]
-
-            df_1.columns = [
-                'seq_name', 'v_string', 'j_string', 'd_string', 'functionality',
-                'v_score', 'j_score', 'vgene_probablity',
-                'dregion_reading_frame', 'cdr1_length', 'cdr2_length',
-                'cdr3_length', 'functionality_comment', 'rev_comp',
-                'vgene_probability'
-            ]
-
-        elif 'V-DOMAIN Functionality' in Summary_column_list:
-
-            df_1 = Summary_1[[
-                'Sequence ID', 'V-GENE and allele', 'J-GENE and allele',
-                'D-GENE and allele', 'V-DOMAIN Functionality',
-                'V-REGION score', 'J-REGION score', 'V-REGION identity %',
-                'D-REGION reading frame', 'CDR1-IMGT length',
-                'CDR2-IMGT length', 'CDR3-IMGT length',
-                'V-DOMAIN Functionality comment', 'Orientation',
-                'V-REGION identity %'
-            ]]
-
-            df_1.columns = [
-                'seq_name', 'v_string', 'j_string', 'd_string', 'functionality',
-                'v_score', 'j_score', 'vgene_probablity',
-                'dregion_reading_frame', 'cdr1_length', 'cdr2_length',
-                'cdr3_length', 'functionality_comment', 'rev_comp',
-                'vgene_probability'
-            ]
-
-        df_2 = IMGT_gapped_nt_sequences_2[[
-            'V-D-J-REGION', 'V-J-REGION', 'V-REGION', 'J-REGION', 'FR1-IMGT',
-            'FR2-IMGT', 'FR3-IMGT', 'FR4-IMGT', 'CDR1-IMGT', 'CDR2-IMGT',
-            'CDR3-IMGT', 'JUNCTION'
-        ]]
-
-        df_2.columns = [
-            'vdjregion_sequence_nt_gapped', 'vjregion_sequence_nt_gapped',
-            'vregion_sequence_nt_gapped', 'jregion_sequence_nt_gapped',
-            'fr1region_sequence_nt_gapped', 'fr2region_sequence_nt_gapped',
-            'fr3region_sequence_nt_gapped', 'fr4region_sequence_nt_gapped',
-            'cdr1region_sequence_nt_gapped', 'cdr2region_sequence_nt_gapped',
-            'cdr3region_sequence_nt_gapped', 'junction_sequence_nt_gapped'
-        ]
-
-        df_3 = Nt_sequences_3[[
-            'V-D-J-REGION', 'V-J-REGION', 'D-J-REGION', 'V-REGION', 'J-REGION',
-            'D-REGION', 'FR1-IMGT', 'FR2-IMGT', 'FR3-IMGT', 'FR4-IMGT',
-            'CDR1-IMGT', 'CDR2-IMGT', 'CDR3-IMGT', 'JUNCTION',
-            'V-D-J-REGION start', 'V-D-J-REGION end', 'V-J-REGION start',
-            'V-J-REGION end', 'V-REGION start', 'V-REGION end',
-            'J-REGION start', 'J-REGION end', 'D-REGION start', 'D-REGION end',
-            'FR1-IMGT start', 'FR1-IMGT end', 'FR2-IMGT start', 'FR2-IMGT end',
-            'FR3-IMGT start', 'FR3-IMGT end', 'FR4-IMGT start', 'FR4-IMGT end',
-            'CDR1-IMGT start', 'CDR1-IMGT end', 'CDR2-IMGT start',
-            'CDR2-IMGT end', 'CDR3-IMGT start', 'CDR3-IMGT end',
-            'JUNCTION start', 'JUNCTION end', 'D-J-REGION start',
-            'D-J-REGION end'
-        ]]
-
-        df_3.columns = [
-            'vdjregion_sequence_nt', 'vjregion_sequence_nt',
-            'djregion_sequence_nt', 'vregion_sequence_nt',
-            'jregion_sequence_nt', 'dregion_sequence_nt',
-            'fr1region_sequence_nt', 'fr2region_sequence_nt',
-            'fr3region_sequence_nt', 'fr4region_sequence_nt',
-            'cdr1region_sequence_nt', 'cdr2region_sequence_nt',
-            'cdr3region_sequence_nt', 'junction_nt', 'vdjregion_start',
-            'vdjregion_end', 'vjregion_start', 'vjregion_end', 'v_start',
-            'v_end', 'j_start', 'j_end', 'd_start', 'd_end', 'fwr1_start',
-            'fwr1_end', 'fwr2_start', 'fwr2_end', 'fwr3_start', 'fwr3_end',
-            'fwr4_start', 'fwr4_end', 'cdr1_start', 'cdr1_end', 'cdr2_start',
-            'cdr2_end', 'cdr3_start', 'cdr3_end', 'junction_start',
-            'junction_end', 'djregion_start', 'djregion_end'
-        ]
-
-        df_4 = IMGT_gapped_AA_sequences_4[[
-            'V-D-J-REGION', 'V-J-REGION', 'V-REGION', 'J-REGION', 'FR1-IMGT',
-            'FR2-IMGT', 'FR3-IMGT', 'FR4-IMGT', 'CDR1-IMGT', 'CDR2-IMGT',
-            'CDR3-IMGT', 'JUNCTION'
-        ]]
-
-        df_4.columns = [
-            'vdjregion_sequence_aa_gapped', 'vjregion_sequence_aa_gapped',
-            'vregion_sequence_aa_gapped', 'jregion_sequence_aa_gapped',
-            'fr1region_sequence_aa_gapped', 'fr2region_sequence_aa_gapped',
-            'fr3region_sequence_aa_gapped', 'fr4region_sequence_aa_gapped',
-            'cdr1region_sequence_aa_gapped', 'cdr2region_sequence_aa_gapped',
-            'cdr3region_sequence_aa_gapped', 'junction_sequence_aa_gapped'
-        ]
-
-        df_5 = AA_sequences_5[[
-            'V-D-J-REGION', 'V-J-REGION', 'V-REGION', 'J-REGION', 'FR1-IMGT',
-            'FR2-IMGT', 'FR3-IMGT', 'FR4-IMGT', 'CDR1-IMGT', 'CDR2-IMGT',
-            'CDR3-IMGT', 'JUNCTION'
-        ]]
-
-        df_5.columns = [
-            'vdjregion_sequence_aa', 'vjregion_sequence_aa',
-            'vregion_sequence_aa', 'jregion_sequence_aa',
-            'fr1region_sequence_aa', 'fr2region_sequence_aa',
-            'fr3region_sequence_aa', 'fr4region_sequence_aa',
-            'cdr1region_sequence_aa', 'cdr2region_sequence_aa',
-            'cdr3region_sequence_aa', 'junction_aa'
-        ]
-
-        df_7 = V_REGION_mutation_and_AA_change_table_7[[
-            'V-REGION', 'FR1-IMGT', 'FR2-IMGT', 'FR3-IMGT', 'CDR1-IMGT',
-            'CDR2-IMGT', 'CDR3-IMGT'
-        ]]
-
-        df_7.columns = [
-            'vregion_mutation_string', 'fr1region_mutation_string',
-            'fr2region_mutation_string', 'fr3region_mutation_string',
-            'cdr1region_mutation_string', 'cdr2region_mutation_string',
-            'cdr3region_mutation_string'
-        ]
+        #df_7.columns = [
+        #    'vregion_mutation_string', 'fr1region_mutation_string',
+        #    'fr2region_mutation_string', 'fr3region_mutation_string',
+        #    'cdr1region_mutation_string', 'cdr2region_mutation_string',
+        #    'cdr3region_mutation_string'
+        #]
 
         #mongo_concat = pd.concat([df_1, df_2, df_3, df_4, df_5, df_7], axis=1)
         # Need to grab some data out of the parameters dictionary.
