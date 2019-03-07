6bd8898a82ac3330066ae2e6016d84e094a9a127
Brian Corrie <bdcorrie@gmail.com>
2019-03-07 00:50:45 +0000

    Added a command line parameter that specifies where the output files go.

--- benchmark/test-explain.sh
+++ benchmark/test-explain.sh
@@ -9,14 +9,15 @@ count=3
 
 # Get the command line arguements, the number of iterations, the DB name,
 # the DB host, and the DB port to use.
-if [ $# -eq 4 ]
+if [ $# -eq 5 ]
 then
     count=$1
     db_name=$2
     db_host=$3
     db_port=$4
+    out_folder=$5
 else
-    echo "usage: $0 count db host port"
+    echo "usage: $0 count db host port out_folder"
     exit
 fi
 
@@ -33,11 +34,11 @@ echo "Test performed at: $current_time"
 
 # Dump the indexes. This is important to know as if the performance
 # is not as good as expected this can help diagnose the problem.
-mongo $db_name --host $db_host --port $db_port $index_js_file > index-$host_name-$db_host-$db_port-$db_name-$current_time.txt
+mongo $db_name --host $db_host --port $db_port $index_js_file > $out_folder/index-$host_name-$db_host-$db_port-$db_name-$current_time.txt
 
 # Dump the query plan cache. This is important to know as if the performance
 # is not as good as expected this can help diagnose the problem.
-mongo $db_name --host $db_host --port $db_port $cache_js_file > cache-$host_name-$db_host-$db_port-$db_name-$current_time.txt
+mongo $db_name --host $db_host --port $db_port $cache_js_file > $out_folder/cache-$host_name-$db_host-$db_port-$db_name-$current_time.txt
 
 # Perform the benchmark test the number of times requested.
 for i in `seq 1 $count`;
@@ -45,7 +46,7 @@ do
     # Run the performance test once. Output file is named such that it is
     # possible to track down where (and when) a performance file came from
     echo "Performing test iteration $i"
-    time mongo $db_name --host $db_host --port $db_port $perf_js_file > run$i-$host_name-$db_host-$db_port-$db_name-$current_time.txt
+    time mongo $db_name --host $db_host --port $db_port $perf_js_file > $out_folder/run$i-$host_name-$db_host-$db_port-$db_name-$current_time.txt
 done
 
 # Print out the time when the test run finished.
