4d7ae8c0ec6679b50ad875ca068c86a2ee2395e1
Brian Corrie <bdcorrie@gmail.com>
2019-11-06 23:20:00 +0000

    Got rid of the generic repertoire parsing code
    Made it so the process method simply prints out an error message
    and returns. All I have to do now is implement it 8-)

--- dataload/airr_repertoire.py
+++ dataload/airr_repertoire.py
@@ -5,53 +5,18 @@ import json
 import os
 from datetime import datetime
 from datetime import timezone
-from parser import Parser
+from repertoire import Repertoire
 
-class Sample(Parser):
+class AIRRRepertoire(Repertoire):
 	
 	def __init__(self,context):
 		self.context = context
-		Parser.__init__(self, context)
+		Repertoire.__init__(self, context)
 		
-	def insertDocument( self, doc ):
-	
-	    cursor = self.context.samples.find( {}, { "_id": 1 } ).sort("_id", -1).limit(1)
-	    
-	    empty = False
-	    
-	    try:
-	        record = cursor.next()
-	        
-	    except StopIteration:
-	        print("Info: No previous record, this is the first insertion")
-	        empty = True
-	        
-	    if empty:
-	        seq = 1
-	    else:
-	        seq = record["_id"]
-	        if not type(seq) is int:
-	            print("ERROR: Invalid ID for samples found, expecting an integer, got " + str(seq))
-	            print("ERROR: DB may be corrupt")
-	            return False
-	        else:
-	            seq = seq+1
-	        
-	    doc["_id"] = seq
-	    if self.context.verbose:
-	        # If we are in verbose mode, print out a summary of the record we are inserting.
-	        study_tag = self.context.airr_map.getMapping("study_id", "ir_id", self.context.repository_tag)
-	        study = "NULL" if not study_tag in doc else doc[study_tag]
-	        sample_tag = self.context.airr_map.getMapping("sample_id", "ir_id", self.context.repository_tag)
-	        sample = "NULL" if not sample_tag in doc else doc[sample_tag]
-	        file_tag = self.context.airr_map.getMapping("ir_rearrangement_file_name", "ir_id", self.context.repository_tag)
-	        filestr = "NULL" if not file_tag in doc else doc[file_tag]
-	        print("Info: Writing repertoire record <%s, %s, %s (ID: %d)>" % (study, sample, filestr, seq))
-
-	    
-	    results = self.context.samples.insert(doc)
-	
 	def process(self, filename):
+		print("AIRRRepertoire: Not yet implemented!")
+		return False
+
 		# Check to see if we have a file	
 		if not os.path.isfile(filename):
 			print("ERROR: input file " + filename + " is not a file")
