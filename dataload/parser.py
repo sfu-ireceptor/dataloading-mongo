8219d18690ac6d04b1b60ed559836d615e8d5c3e
Brian Corrie <bdcorrie@gmail.com>
2019-12-06 22:15:37 +0000

    Added getMapping() method. Added a couple of other utility methods as well...

--- dataload/parser.py
+++ dataload/parser.py
@@ -26,9 +26,24 @@ class Parser:
     def getDateTimeNowUTC():
         return datetime.now(timezone.utc).strftime("%a %b %d %Y %H:%M:%S %Z")
 
+    # Utility method to return the tag used by the mapping for the repository.
+    def getRepositoryTag(self):
+        return self.context.repository_tag
+
+    # Utility method to return the size of the data set chunks to load in
+    # the repository. 
+    def getRepositoryChunkSize(self):
+        return self.context.repository_chunk
+
+    # Utility method to return the verbose boolena flag - all parsers need this.
     def verbose(self):
         return self.context.verbose
 
+    # Utility method to perform the mapping - all parser's need this capability.
+    def getMapping(self, field, from_column, to_column):
+        return self.context.airr_map.getMapping(field, from_column, to_column)
+
+    # Utility methods to deal with data and scratch folders that all Parsers need.
     def getDataFolder(self, fileWithPath):
         # Data folder is based on the path to the file.
         data_dir = os.path.dirname(fileWithPath)
@@ -36,7 +51,6 @@ class Parser:
         if (not os.path.isdir(data_dir)):
             data_dir = "."
         return data_dir
-
 
     # We create a specific temporary 'scratch' folder for each sequence archive
     def setScratchFolder(self, fileWithPath, fileName):
