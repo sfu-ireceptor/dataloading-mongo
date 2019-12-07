fe350b59434740a088c85be66dfac04c6871c8e4
Brian Corrie <bdcorrie@gmail.com>
2019-12-07 00:31:12 +0000

    Refactored to use new class functions and get rid of most context references.

--- dataload/mixcr.py
+++ dataload/mixcr.py
@@ -38,7 +38,7 @@ class MiXCR(Rearrangement):
         filename = os.path.basename(filewithpath)
 
         if filewithpath.endswith(".gz"):
-            if self.context.verbose:
+            if self.verbose():
                 print("Info: Reading data gzip archive: "+filewithpath)
             with gzip.open(filewithpath, 'rb') as file_handle:
                 # read file directly from the file handle 
@@ -46,7 +46,7 @@ class MiXCR(Rearrangement):
                 success = self.processMiXcrFile(file_handle, filename)
 
         else: # read directly as a regular text file
-            if self.context.verbose:
+            if self.verbose():
                 print("Info: Reading text file: "+filewithpath)
             file_handle = open(filewithpath, "r")
             success = self.processMiXcrFile(file_handle, filename)
@@ -58,29 +58,29 @@ class MiXCR(Rearrangement):
         # Set the tag for the repository that we are using. Note this should
         # be refactored so that it is a parameter provided so that we can use
         # multiple repositories.
-        repository_tag = self.context.repository_tag
+        repository_tag = self.getRepositoryTag()
 
         # Set the tag for the file mapping that we are using. Ths is essentially the
         # look up into the columns of the AIRR Mapping that we are using. 
         filemap_tag = self.getFileMapping()
 
         # Define the number of records to iterate over
-        chunk_size = self.context.repository_chunk
+        chunk_size = self.getRepositoryChunkSize()
 
         # Query for the sample and create an array of sample IDs
         filename = filename.replace(".gz", "")
 
         # Get the sample ID of the data we are processing. We use the IMGT file name for
         # this at the moment, but this may not be the most robust method.
-        value = self.context.airr_map.getMapping("ir_rearrangement_file_name", "ir_id", repository_tag)
+        value = self.getMapping("ir_rearrangement_file_name", "ir_id", repository_tag)
         idarray = []
         if value is None:
             print("ERROR: Could not find ir_rearrangement_file_name in repository " + repository_tag)
             return False
         else:
-            if self.context.verbose:
+            if self.verbose():
                 print("Info: Retrieving associated sample for file " + filename + " from repository field " + value)
-            idarray = Rearrangement.getSampleIDs(self.context, value, filename)
+            idarray = self.repositoryGetSampleIDs(value, filename)
 
 
         # Check to see that we found it and that we only found one. Fail if not.
@@ -112,11 +112,11 @@ class MiXCR(Rearrangement):
         # want to extract fields that the repository doesn't want.
         mixcrColumns = []
         columnMapping = {}
-        if self.context.verbose:
+        if self.verbose():
             print("Info: Dumping expected " + self.getAnnotationTool() + "(" + filemap_tag +
                   ") to repository mapping")
         for index, row in file_fields.iterrows():
-            if self.context.verbose:
+            if self.verbose():
                 print("Info:    " + str(row[filemap_tag]) + " -> " + str(row[repository_tag]))
             # If the repository column has a value for the IMGT field, track the field
             # from both the IMGT and repository side.
@@ -124,7 +124,7 @@ class MiXCR(Rearrangement):
                 mixcrColumns.append(row[filemap_tag])
                 columnMapping[row[filemap_tag]] = row[repository_tag]
             else:
-                if self.context.verbose:
+                if self.verbose():
                     print("Info:    Repository does not support " +
                           str(row[filemap_tag]) + ", not inserting into repository")
 
@@ -134,7 +134,7 @@ class MiXCR(Rearrangement):
         # we don't want to map empty strings to Pandas NaN values. This causes an issue as
         # missing strings get read as a NaN value, which is interpreted as a string. One can
         # then not tell the difference between a "nan" string and a "NAN" Junction sequence.
-        if self.context.verbose:
+        if self.verbose():
             print("Info: Preparing the file reader...", flush=True)
         df_reader = pd.read_csv(file_handle, sep='\t', chunksize=chunk_size, na_filter=False)
 
@@ -142,7 +142,7 @@ class MiXCR(Rearrangement):
         total_records = 0
         for df_chunk in df_reader:
 
-            if self.context.verbose:
+            if self.verbose():
                 print("Info: Processing raw data frame...", flush=True)
             # Remap the column names. We need to remap because the columns may be in a differnt
             # order in the file than in the column mapping. We leave any non-mapped columns in the
@@ -150,39 +150,39 @@ class MiXCR(Rearrangement):
             for mixcr_column in df_chunk.columns:
                 if mixcr_column in columnMapping:
                     mongo_column = columnMapping[mixcr_column]
-                    if self.context.verbose:
+                    if self.verbose():
                         print("Info: Mapping " + self.getAnnotationTool() + " field in file: " +
                               mixcr_column + " -> " + mongo_column)
                     df_chunk.rename({mixcr_column:mongo_column}, axis='columns', inplace=True)
                 else:
-                    if self.context.verbose:
+                    if self.verbose():
                         print("Info: No mapping for " + self.getAnnotationTool() + " input file column " +
                               mixcr_column + ", storing in repository as is")
             # Check to see which desired MiXCR mappings we don't have...
             for mixcr_column, mongo_column in columnMapping.items():
                 if not mongo_column in df_chunk.columns:
-                    if self.context.verbose:
+                    if self.verbose():
                         print("Info: Missing data in input " + self.getAnnotationTool() + 
                               " file for " + mixcr_column)
             
             # Build the substring array that allows index for fast searching of
             # Junction AA substrings. Also calculate junction AA length
-            junction_aa = self.context.airr_map.getMapping("junction_aa", "ir_id", repository_tag)
-            ir_substring = self.context.airr_map.getMapping("ir_substring", "ir_id", repository_tag)
-            ir_junction_aa_length = self.context.airr_map.getMapping("ir_junction_aa_length", "ir_id", repository_tag)
+            junction_aa = self.getMapping("junction_aa", "ir_id", repository_tag)
+            ir_substring = self.getMapping("ir_substring", "ir_id", repository_tag)
+            ir_junction_aa_length = self.getMapping("ir_junction_aa_length", "ir_id", repository_tag)
             if junction_aa in df_chunk:
-                if self.context.verbose:
+                if self.verbose():
                     print("Info: Computing junction amino acids substrings...", flush=True)
                 df_chunk[ir_substring] = df_chunk[junction_aa].apply(Rearrangement.get_substring)
-                if self.context.verbose:
+                if self.verbose():
                     print("Info: Computing junction amino acids length...", flush=True)
                 df_chunk[ir_junction_aa_length] = df_chunk[junction_aa].apply(str).apply(len)
 
             # MiXCR doesn't have junction nucleotide length, we want it in our repository.
-            junction = self.context.airr_map.getMapping("junction", "ir_id", repository_tag)
-            junction_length = self.context.airr_map.getMapping("junction_length", "ir_id", repository_tag)
+            junction = self.getMapping("junction", "ir_id", repository_tag)
+            junction_length = self.getMapping("junction_length", "ir_id", repository_tag)
             if junction in df_chunk:
-                if self.context.verbose:
+                if self.verbose():
                     print("Info: Computing junction length...", flush=True)
                 df_chunk[junction_length] = df_chunk[junction].apply(str).apply(len)
 
@@ -190,40 +190,40 @@ class MiXCR(Rearrangement):
             # We need to look up the "known parameter" from an iReceptor perspective (the field
             # name in the "ir_id" column mapping and map that to the correct field name for the
             # repository we are writing to.
-            v_call = self.context.airr_map.getMapping("v_call", "ir_id", repository_tag)
-            d_call = self.context.airr_map.getMapping("d_call", "ir_id", repository_tag)
-            j_call = self.context.airr_map.getMapping("j_call", "ir_id", repository_tag)
-            ir_vgene_gene = self.context.airr_map.getMapping("ir_vgene_gene", "ir_id", repository_tag)
-            ir_dgene_gene = self.context.airr_map.getMapping("ir_dgene_gene", "ir_id", repository_tag)
-            ir_jgene_gene = self.context.airr_map.getMapping("ir_jgene_gene", "ir_id", repository_tag)
-            ir_vgene_family = self.context.airr_map.getMapping("ir_vgene_family", "ir_id", repository_tag)
-            ir_dgene_family = self.context.airr_map.getMapping("ir_dgene_family", "ir_id", repository_tag)
-            ir_jgene_family = self.context.airr_map.getMapping("ir_jgene_family", "ir_id", repository_tag)
+            v_call = self.getMapping("v_call", "ir_id", repository_tag)
+            d_call = self.getMapping("d_call", "ir_id", repository_tag)
+            j_call = self.getMapping("j_call", "ir_id", repository_tag)
+            ir_vgene_gene = self.getMapping("ir_vgene_gene", "ir_id", repository_tag)
+            ir_dgene_gene = self.getMapping("ir_dgene_gene", "ir_id", repository_tag)
+            ir_jgene_gene = self.getMapping("ir_jgene_gene", "ir_id", repository_tag)
+            ir_vgene_family = self.getMapping("ir_vgene_family", "ir_id", repository_tag)
+            ir_dgene_family = self.getMapping("ir_dgene_family", "ir_id", repository_tag)
+            ir_jgene_family = self.getMapping("ir_jgene_family", "ir_id", repository_tag)
 
             # Build the v_call field, as an array if there is more than one gene
             # assignment made by the annotator.
-            Rearrangement.processGene(self.context, df_chunk, v_call, v_call, ir_vgene_gene, ir_vgene_family)
-            Rearrangement.processGene(self.context, df_chunk, j_call, j_call, ir_jgene_gene, ir_jgene_family)
-            Rearrangement.processGene(self.context, df_chunk, d_call, d_call, ir_dgene_gene, ir_dgene_family)
+            self.processGene(df_chunk, v_call, v_call, ir_vgene_gene, ir_vgene_family)
+            self.processGene(df_chunk, j_call, j_call, ir_jgene_gene, ir_jgene_family)
+            self.processGene(df_chunk, d_call, d_call, ir_dgene_gene, ir_dgene_family)
             # If we don't already have a locus (that is the data file didn't provide one) then
             # calculate the locus based on the v_call array.
-            locus = self.context.airr_map.getMapping("locus", "ir_id", repository_tag)
+            locus = self.getMapping("locus", "ir_id", repository_tag)
             if not locus in df_chunk:
                 df_chunk[locus] = df_chunk[v_call].apply(Rearrangement.getLocus)
 
             # Assign each record the constant fields for all records in the chunk
-            productive = self.context.airr_map.getMapping("productive", "ir_id", repository_tag)
+            productive = self.getMapping("productive", "ir_id", repository_tag)
             df_chunk[productive] = 1
             # Assign any iReceptor specific custom fields for the records in the chunk
-            ir_annotation_tool = self.context.airr_map.getMapping("ir_annotation_tool", "ir_id", repository_tag)
+            ir_annotation_tool = self.getMapping("ir_annotation_tool", "ir_id", repository_tag)
             df_chunk[ir_annotation_tool] = self.getAnnotationTool()
-            ir_project_sample_id_field = self.context.airr_map.getMapping("ir_project_sample_id", "ir_id", repository_tag)
+            ir_project_sample_id_field = self.getMapping("ir_project_sample_id", "ir_id", repository_tag)
             df_chunk[ir_project_sample_id_field] = ir_project_sample_id
             # Create the created and update values for this block of records. Note that this
             # means that each block of inserts will have the same date.
             now_str = Rearrangement.getDateTimeNowUTC()
-            ir_created_at = self.context.airr_map.getMapping("ir_created_at", "ir_id", repository_tag)
-            ir_updated_at = self.context.airr_map.getMapping("ir_updated_at", "ir_id", repository_tag)
+            ir_created_at = self.getMapping("ir_created_at", "ir_id", repository_tag)
+            ir_updated_at = self.getMapping("ir_updated_at", "ir_id", repository_tag)
             df_chunk[ir_created_at] = now_str
             df_chunk[ir_updated_at] = now_str
 
@@ -232,7 +232,7 @@ class MiXCR(Rearrangement):
             print("Info: Inserting", num_records, "records into Mongo...", flush=True)
             t_start = time.perf_counter()
             records = json.loads(df_chunk.T.to_json()).values()
-            self.context.sequences.insert_many(records)
+            self.repositoryWriteRearrangements(records)
             t_end = time.perf_counter()
             print("Info: Inserted records, time =", (t_end - t_start), "seconds", flush=True)
 
@@ -241,16 +241,12 @@ class MiXCR(Rearrangement):
             print("Info: Total records so far =", total_records, flush=True)
 
         # Get the number of annotations for this repertoire (as defined by the ir_project_sample_id)
-        if self.context.verbose:
+        if self.verbose():
             print("Info: Getting the number of annotations for this repertoire")
-        annotation_count = self.context.sequences.find(
-                {ir_project_sample_id_field:{'$eq':ir_project_sample_id}}
-            ).count()
+        annotation_count = self.repositoryCountRearrangements(ir_project_sample_id)
 
         # Set the cached ir_sequeunce_count field for the repertoire/sample.
-        self.context.samples.update(
-            {"_id":ir_project_sample_id}, {"$set": {"ir_sequence_count":annotation_count}}
-        )
+        self.repositoryUpdateCount(ir_project_sample_id, annotation_count)
 
         # Inform on what we added and the total count for the this record.
         print("Info: Inserted %d records, total annotation count = %d" % (total_records, annotation_count))
