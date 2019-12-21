a1e231f4a74c16fbe4fec66b82dd0f330011efcb
Brian Corrie <bdcorrie@gmail.com>
2019-12-21 02:13:51 +0000

    Added some fields and methods to track the fields we use to link repertoire and rearrangements. Trying to get rid of referring to those fields in the subclasses so they are defined only in one place.

--- dataload/parser.py
+++ dataload/parser.py
@@ -35,10 +35,45 @@ class Parser:
         # Save the AIRR Mapping object
         self.airr_map = airr_map
 
+        # Keep track of the fields that we use to link the two types of files
+        # that we parse, repertoire and rearrangement files. When we parse
+        # rearrangement files, we assume that all of the rearrangements in each
+        # file belong to a single repertoire, although it is possible to have more
+        # than one file contain rearrangements of the same repertoire.
+        #
+        # As a result, when we load a file of rearrangements we need to associate
+        # each file with a repertoire. There are two ways of doing this.
+        # 
+        # Firstly, each row in the Repertoire collection has a repertoire ID field
+        # (identified by a specific ir_id field) and it is this field 
+        # that must be unique across all of the repertoires in the repository. As
+        # a result, it is possible to specify a repertoire ID to which all of the
+        # rearrangements in a specific file should be associated.
+        #
+        # Secondly, it is possible to use the rearrangement file name of the file 
+        # being loaded to identify the repertoire to which the rearrangements
+        # belong. The file name for rearrangements is stored in a field (again,
+        # identified by a specific ir_id field).
+        #
+        # Below, we keep track of both of these important fields. This is
+        # maintained by the Parser class because these are the fields that link
+        # the two types of parsed files.
+        self.repertoire_linkid_field = "ir_project_sample_id"
+        self.rearrangement_file_field = "ir_rearrangement_file_name"
 
-    @staticmethod
-    def getDateTimeNowUTC():
-        return datetime.now(timezone.utc).strftime("%a %b %d %Y %H:%M:%S %Z")
+        # Finally, we need to keep track of the field (identified by an ir_id
+        # field name in the rearrangement collection that points to the
+        # Repertoire ID field in the repertoire collection.
+        self.rearrangement_linkid_field = "repertoire_id"
+
+    def getRepertoireLinkIDField(self):
+        return self.repertoire_linkid_field
+
+    def getRearrangementFileField(self):
+        return self.rearrangement_file_field
+
+    def getRearrangementLinkIDField(self):
+        return self.rearrangement_linkid_field
 
     # Utility method to return the tag used by the mapping for the repository.
     def getRepositoryTag(self):
@@ -73,3 +108,8 @@ class Parser:
 
     def getScratchFolder(self):
         return self.scratchFolder
+
+    @staticmethod
+    def getDateTimeNowUTC():
+        return datetime.now(timezone.utc).strftime("%a %b %d %Y %H:%M:%S %Z")
+
