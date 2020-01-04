3b62035a7564591deff25ea4979c83c244ea9f2d
Brian Corrie <bdcorrie@gmail.com>
2020-01-04 21:40:17 +0000

    Cleaned up the field and type mapping code a fair bit...

--- dataload/ir_repertoire.py
+++ dataload/ir_repertoire.py
@@ -21,6 +21,39 @@ class IRRepertoire(Repertoire):
 
         # Set the tag for the repository that we are using.
         repository_tag = self.getRepositoryTag()
+        # Check to see if we have the columns needed in the mapping, if not exit.
+        if not self.getAIRRMap().hasColumn(repository_tag):
+            print("ERROR: Could not find Repository mapping (%s) in mapping file"%
+                  (repository_tag))
+            return False
+
+        curation_tag = "ir_curator"
+        # Check to see if we have the columns needed in the mapping, if not exit.
+        if not self.getAIRRMap().hasColumn(curation_tag):
+            print("ERROR: Could not find Curation mapping (%s) in mapping file"%
+                  (curation_tag))
+            return False
+
+        airr_tag = "airr"
+        # Check to see if we have the columns needed in the mapping, if not exit.
+        if not self.getAIRRMap().hasColumn(airr_tag):
+            print("ERROR: Could not find AIRR mapping (%s) in mapping file"%
+                  (airr_tag))
+            return False
+
+        airr_type_tag = "airr_type"
+        # Check to see if we have the columns needed in the mapping, if not exit.
+        if not self.getAIRRMap().hasColumn(airr_type_tag):
+            print("ERROR: Could not find AIRR type mapping (%s) in mapping file"%
+                  (airr_type_tag))
+            return False
+
+        repository_type_tag = "ir_repository_type"
+        # Check to see if we have the columns needed in the mapping, if not exit.
+        if not self.getAIRRMap().hasColumn(repository_type_tag):
+            print("ERROR: Could not find Repository type mapping (%s) in mapping file"%
+                  (repository_type_tag))
+            return False
 
         # Get the fields to use for finding repertoire IDs, either using those IDs
         # directly or by looking for a repertoire ID based on a rearrangement file
@@ -28,35 +61,45 @@ class IRRepertoire(Repertoire):
         repertoire_id_field = self.getRepertoireLinkIDField()
         rearrangement_file_field = self.getRearrangementFileField()
 
-        # Extract the fields that are of interest for this file. Essentiall all
-        # non null curator fields
-        curation_tag = "ir_curator"
-        if not self.getAIRRMap().hasColumn(curation_tag):
-            print("ERROR: Could not find Curation mapping (%s) in mapping file"%
-                  (curation_tag))
-            return False
-        map_column = self.getAIRRMap().getRepertoireMapColumn(curation_tag)
-        map_column = self.getAIRRMap().airr_mappings[curation_tag]
+        # Get the column of values from the AIRR tag. We only want the
+        # Repertoire related fields.
+        map_column = self.getAIRRMap().getRepertoireMapColumn(airr_tag)
+        # Get a boolean column that flags columns of interest. Exclude nulls.
         fields_of_interest = map_column.notnull()
-        
-        # We select the rows in the mapping that contain fields of interest for curataion.
-        # At this point, file_fields contains N columns that contain our mappings for the
-        # the specific formats (e.g. ir_id, airr, vquest). The rows are limited to have
-        # only data that is relevant to curataion
+        # Afer the following, airr_fields contains N columns (e.g. ir_id, airr, vquest) 
+        # that contain the AIRR Repertoire mappings. 
         airr_fields = self.getAIRRMap().getRepertoireRows(fields_of_interest)
-        curator_fields = self.getAIRRMap().airr_mappings.loc[fields_of_interest]
+
+        # Get the fields (as above) for the iReceptor Curator mapping.
+        map_column = self.getAIRRMap().getIRRepertoireMapColumn(curation_tag)
+        fields_of_interest = map_column.notnull()
+        curator_fields = self.getAIRRMap().getIRRepertoireRows(fields_of_interest)
+
+        # If we are in verbose mode, dump out the AIRR mapping.
+        if self.verbose():
+            print("Info: Dumping AIRR repertoire mapping")
+            for index, row in airr_fields.iterrows():
+                print("Info:    %s -> %s"%
+                      (str(row[airr_tag]), str(row[repository_tag])))
         
         # We need to build a column mapping for the curation columns to the fields
         # that the repository will store.
-        columnMapping = {}
-
-        if self.verbose():
-            print("Info: Dumping AIRR repertoire mapping")
+        columnMapping = dict()
+        curationType = dict()
+        repositoryType = dict()
         type_dict = dict()
-        for index, row in airr_fields.iterrows():
-            if self.verbose():
-                print("Info:    %s -> %s"%
-                      (str(row[curation_tag]), str(row[repository_tag])))
+        na_dict = dict()
+        if self.verbose():
+            print("Info: Building field and type mappings (curator -> repository)")
+        for index, row in curator_fields.iterrows():
+            curation_field = row[curation_tag]
+            # Get the types of the fields, both the AIRR type and the repository type
+            curation_field_type = self.getAIRRMap().getMapping(curation_field,
+                                           curation_tag, airr_type_tag,
+                                           self.getAIRRMap().getIRRepertoireClass())
+            repository_field_type = self.getAIRRMap().getMapping(curation_field,
+                                           curation_tag, repository_type_tag,
+                                           self.getAIRRMap().getIRRepertoireClass())
 
             # For each column, get a type value and if it is integer or string set the
             # type in the type dictionary. We use this later when we read the CSV file
@@ -67,24 +110,31 @@ class IRRepertoire(Repertoire):
             # See:
             #   - https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html
             #   - https://pandas.pydata.org/pandas-docs/stable/user_guide/integer_na.html
-            field_type = self.getAIRRMap().getMapping(row[curation_tag],
-                                           "airr", "airr_type",
-                                           self.getAIRRMap().getRepertoireClass())
-            if field_type == "integer":
-                type_dict[row[curation_tag]] = "Int64"
-            elif field_type == "string":
-                type_dict[row[curation_tag]] = str
-            elif field_type == "number":
-                type_dict[row[curation_tag]] = float
-            elif field_type == "boolean":
-                type_dict[row[curation_tag]] = bool
+            if curation_field_type == "integer":
+                type_dict[curation_field] = "Int64"
+            elif curation_field_type == "string":
+                #print("%s %s"%(curation_field, curation_field_type))
+                type_dict[curation_field] = str
+                #na_dict[curation_field] = None
+            elif curation_field_type == "number":
+                type_dict[curation_field] = float
+            #elif curation_field_type == "boolean":
+            #    # We treat boolean as Int64 when we read them in so we can manage
+            #    # NULL boolean values.
+            #    type_dict[curation_field] = "Int64"
 
-            # If the repository column has a value for the AIRR field, track the field
+            # If the repository column has a value for the curator field, track the field
             # from both the curator and repository side.
             if not pd.isnull(row[repository_tag]):
-                columnMapping[row[curation_tag]] = row[repository_tag]
+                columnMapping[curation_field] = row[repository_tag]
+                curationType[curation_field] = curation_field_type
+                repositoryType[curation_field] = repository_field_type
+                if self.verbose():
+                    print("Info:    %s (%s)-> %s (%s)"%
+                          (str(curation_field), curationType[curation_field],
+                           str(row[repository_tag]), repositoryType[curation_field]))
             else:
-                print("Warning: No AIRR mapping for field %s"%(str(row[curation_tag])))
+                print("Warning: No Curation mapping for field %s"%(str(curation_field)))
 
         # Read in the CSV file. We need to read this with a utf-8-sig encoding,
         # which means it is a UTF file with a BOM signature. Note that this has
@@ -93,8 +143,10 @@ class IRRepertoire(Repertoire):
         # cause Pandas to complain if the typing is wrong.
         try:
             df = pd.read_csv(filename, sep=None, engine='python',
-                             encoding='utf-8-sig')
-                             #encoding='utf-8-sig', dtype=type_dict)
+                             encoding='utf-8-sig',
+                             dtype=type_dict)
+                             #dtype=type_dict, na_values=na_dict)
+                             #encoding='utf-8-sig')
         except Exception as err:
             print("ERROR: Unable to open file %s - %s" % (filename, err))
             return False
@@ -106,6 +158,8 @@ class IRRepertoire(Repertoire):
         if (df.columns.str.contains('^Unnamed').any()):
             print("Warning: column without a title detected in file ", filename)    
         df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+        print(df["physical_linkage"])
+        print(df["grants"])
 
         # Check to make sure all of the curator_fields are present in the file read in.
         # If not, this is an error.
@@ -125,12 +179,13 @@ class IRRepertoire(Repertoire):
         for (curation_file_column, column_data) in df.iteritems():
             # For each column, check the value of the data against the type expected
             field_type = self.getAIRRMap().getMapping(curation_file_column,
-                                           "airr", "airr_type",
-                                           self.getAIRRMap().getRepertoireClass())
+                                           airr_tag, airr_type_tag,
+                                           self.getAIRRMap().getIRRepertoireClass())
             # Skip ontology fields and array fields for now.
             if not field_type in ["ontology", "array"]:
                 value = column_data[0]
                 if not self.validAIRRFieldType(curation_file_column, value, False):
+                    print(value)
                     print("Warning: Found type mismatch in column %s"%
                           (curation_file_column))
                     bad_columns.append(curation_file_column)
