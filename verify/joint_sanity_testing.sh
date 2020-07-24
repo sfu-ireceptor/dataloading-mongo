5e05fb77bda79dc46cb65063a82c496752f1e754
Laura Gutierrez Funderburk <lfunderburk@users.noreply.github.com>
2020-07-23 20:23:28 -0700

    correct indeces

--- verify/joint_sanity_testing.sh
+++ verify/joint_sanity_testing.sh
@@ -22,29 +22,29 @@ echo " "
 echo "Generate facet queries"
 echo ""
 
-# $1 base_url       String containing URL to API server (e.g. https://airr-api2.ireceptor.org)
-# $2 entry_point    Options: string 'rearragement' or string 'repertoire'
-# $3 path_to_json   Enter full path to JSON directory where facet JSON query files will be stored
-# $4 no_filters     Enter full path to JSON query nofilters
-# $5 study_id       Enter study_id
+# $0 base_url       String containing URL to API server (e.g. https://airr-api2.ireceptor.org)
+# $1 entry_point    Options: string 'rearragement' or string 'repertoire'
+# $2 path_to_json   Enter full path to JSON directory where facet JSON query files will be stored
+# $3 no_filters     Enter full path to JSON query nofilters
+# $4 study_id       Enter study_id
 
-python3 /app/verify/generate_facet_json.py "$1" "$2" "$3" "$4" "$5"
+python3 /app/verify/generate_facet_json.py "$0" "$1" "$2" "$3" "$4"
 
 echo "Begin sanity check"
 echo ""
 
-# $6 mapping_file    Indicate the full path to where the mapping file is found
-# $1 base_url        String containing URL to API server (e.g. https://airr-api2.ireceptor.org)
-# $2 entry_point     Options: string 'rearragement' or string 'repertoire'
-# $4 no_filters      Enter full path to JSON query nofilters
-# $7 master_md       Full path to master metadata
-# $5 study_id        Study ID (study_id) associated to this study
-# $3 path_to_json    Enter full path to JSON directory where facet JSON query files will be stored
-# $8 annotation_dir  Enter full path to where annotation files associated with study_id
-# $9 details_dir     Enter full path where you'd like to store content feedback in CSV format
+# $5 mapping_file    Indicate the full path to where the mapping file is found
+# $0 base_url        String containing URL to API server (e.g. https://airr-api2.ireceptor.org)
+# $1 entry_point     Options: string 'rearragement' or string 'repertoire'
+# $3 no_filters      Enter full path to JSON query nofilters
+# $6 master_md       Full path to master metadata
+# $4 study_id        Study ID (study_id) associated to this study
+# $2 path_to_json    Enter full path to JSON directory where facet JSON query files will be stored
+# $7 annotation_dir  Enter full path to where annotation files associated with study_id
+# $8 details_dir     Enter full path where you'd like to store content feedback in CSV format
 
 
-python3 /app/verify/AIRR-repertoire-checks.py "$6" "$1" "$2" "$4" "$7" "$5" "$3" "$8" "$9"  
+python3 /app/verify/AIRR-repertoire-checks.py "$5" "$0" "$1" "$3" "$6" "$4" "$2" "$7" "$8"  
 
 echo "End Script" 
 
