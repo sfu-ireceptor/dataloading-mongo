9a26b342bd59936a8a613705e3c6d678555d1d50
Laura Gutierrez Funderburk <lfunderburk@users.noreply.github.com>
2020-08-08 17:35:11 -0700

    add echo feedback

--- verify/joint_sanity_testing.sh
+++ verify/joint_sanity_testing.sh
@@ -28,20 +28,36 @@ echo ""
 # $4 no_filters     Enter full path to JSON query nofilters
 # $5 study_id       Enter study_id
 
+echo "base_url" "$1" 
+echo "entry_point" "$2" 
+echo "path_to_json" "$3" 
+echo "no_filters" "$4" 
+echo "study_id" "$5"
 python3 /app/verify/generate_facet_json.py "$1" "$2" "$3" "$4" "$5"
 
 echo "Begin sanity check"
 echo ""
 
-# $5 mapping_file    Indicate the full path to where the mapping file is found
-# $0 base_url        String containing URL to API server (e.g. https://airr-api2.ireceptor.org)
-# $1 entry_point     Options: string 'rearragement' or string 'repertoire'
-# $3 no_filters      Enter full path to JSON query nofilters
-# $6 master_md       Full path to master metadata
-# $4 study_id        Study ID (study_id) associated to this study
-# $2 path_to_json    Enter full path to JSON directory where facet JSON query files will be stored
-# $7 annotation_dir  Enter full path to where annotation files associated with study_id
-# $8 details_dir     Enter full path where you'd like to store content feedback in CSV format
+# $6 mapping_file    Indicate the full path to where the mapping file is found
+# $1 base_url        String containing URL to API server (e.g. https://airr-api2.ireceptor.org)
+# $2 entry_point     Options: string 'rearragement' or string 'repertoire'
+# $4 no_filters      Enter full path to JSON query nofilters
+# $7 master_md       Full path to master metadata
+# $5 study_id        Study ID (study_id) associated to this study
+# $3 path_to_json    Enter full path to JSON directory where facet JSON query files will be stored
+# $8 annotation_dir  Enter full path to where annotation files associated with study_id
+# $9 details_dir     Enter full path where you'd like to store content feedback in CSV format
+
+echo "mapping_file" "$6" 
+echo "Base url" "$1" 
+echo "entry point" "$2" 
+echo "No filters" "$4" 
+echo "master md" "$7" 
+echo "Study id" "$5" 
+echo "Path to json" "$3" 
+echo "annotation dir" "$8" 
+echo "details dir" "$9" 
+echo "Sanity check level" "CC-FC"
 
 
 python3 /app/verify/AIRR-repertoire-checks.py "$6" "$1" "$2" "$4" "$7" "$5" "$3" "$8" "$9" "CC-FC"
