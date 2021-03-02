afb20f502d5fc23c638b3239016b863e1957f6f1
bcorrie <bdcorrie@gmail.com>
2021-03-02 00:34:53 +0000

    Got rid of some debug output. Also made the shell script run python on code in the shell script directory. Makes it possible to run in the container but not hard code the container app path.

--- verify/joint_sanity_testing.sh
+++ verify/joint_sanity_testing.sh
@@ -7,18 +7,13 @@
 # LAST MODIFIED ON: July 22, 2020
 # This script generates facet queries (JSON files) and performs repertoire sanity check
 
-TIME1=`date +%Y-%m-%d_%H-%M-%S`
 
-echo "Current working directory: `pwd`"
-echo "Starting run at: " ${TIME1}
-
+# Get the directory where the script is. That is where the python code will be.
+SCRIPT_DIR=`dirname "$0"`
 
 # ---------------------------------------------------------------------
-
-echo "Begin Script"
-echo $PWD
-echo " "
-
+TIME=`date +%Y-%m-%d_%H-%M-%S`
+echo "Starting run at: " ${TIME}
 echo "Generate facet queries"
 echo ""
 
@@ -28,13 +23,9 @@ echo ""
 # $4 no_filters     Enter full path to JSON query nofilters
 # $5 study_id       Enter study_id
 
-echo "base_url" "$1" 
-echo "entry_point" "$2" 
-echo "path_to_json" "$3" 
-echo "no_filters" "$4" 
-echo "study_id" "$5"
-python3 /app/verify/generate_facet_json.py "$1" "$2" "$3" "$4" "$5"
+python3 $SCRIPT_DIR/generate_facet_json.py -v "$1" "$2" "$3" "$4" "$5"
 
+echo ""
 echo "Begin sanity check"
 echo ""
 
@@ -48,18 +39,6 @@ echo ""
 # $8 annotation_dir  Enter full path to where annotation files associated with study_id
 # $9 details_dir     Enter full path where you'd like to store content feedback in CSV format
 
-echo "mapping_file" "$6" 
-echo "Base url" "$1" 
-echo "entry point" "$2" 
-echo "No filters" "$4" 
-echo "master md" "$7" 
-echo "Study id" "$5" 
-echo "Path to json" "$3" 
-echo "annotation dir" "$8" 
-echo "details dir" "$9" 
-echo "Sanity check level" "CC-FC"
-
-
-python3 /app/verify/AIRR-repertoire-checks.py "$6" "$1" "$2" "$4" "$7" "$5" "$3" "$8" "$9" "CC-FC"
-
-echo "End Script" 
+python3 $SCRIPT_DIR/AIRR-repertoire-checks.py "$6" "$1" "$2" "$4" "$7" "$5" "$3" "$8" "$9" "CC-FC"
+TIME=`date +%Y-%m-%d_%H-%M-%S`
+echo "Ending run at: " ${TIME}
