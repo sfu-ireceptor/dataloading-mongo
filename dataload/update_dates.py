8b472827639f95f89bf784836822a3b8051a1339
bzimonja <30536574+bzimonja@users.noreply.github.com>
2022-09-26 20:08:23 -0700

    fixes to improve user experience
    -add 'noupdate' flag to update_dates.py to test if update will happen or not
    -update ir_updated_at with current time if keywords_study needs updating.

--- dataload/update_dates.py
+++ dataload/update_dates.py
@@ -26,9 +26,24 @@ insert_date_field = sys.argv[4]
 update_date_field = sys.argv[5]
 db_date_format = sys.argv[6]
 
-#filename = "master_metadata_20171031.csv"
+# optional 'noupdate' argument if user wants to test what an update would do
+#   without affecting the database
+try: 
+    no_update = sys.argv[7]
+except:
+    no_update = False
 
-def updateDocument(doc, targetCollections):
+if (no_update == "noupdate"):
+    no_update = True 
+else:
+    no_update = False
+
+# list of names that are used for rearrangement collection
+rearrangement_names = ["sequence", "sequences", "rearrangement", "rearrangements"]
+
+#function to check if a document insert or update date (if it exists) matches 
+#  the provided date format, if not, report error
+def check_document(doc):
     id = doc["_id"]
 
     # the "added to database" field may or may not exist
@@ -38,6 +53,55 @@ def updateDocument(doc, targetCollections):
     except:
         old_insert_date = None
 
+    try:
+        old_update_date = doc[update_date_field]
+    except:
+        old_update_date = None
+
+    if (old_insert_date is not None):
+        try:
+            old_insert_datetime = datetime.datetime.strptime(old_insert_date, db_date_format)
+            new_insert_date = old_insert_datetime.isoformat()
+            print ("Document ", id, " insert date ", old_insert_date, " will change to ", new_insert_date, 
+                flush=True)
+        except:
+            print ("Document ", id," insert date ",old_insert_date ," does not match the format provided",
+                flush=True)
+    else:
+        print ("Document ", id," does not have the insert field ", insert_date_field, flush=True)
+
+    new_update_date = datetime.datetime.now().isoformat()
+    if (old_update_date is not None):
+        try:
+            old_update_datetime = datetime.datetime.strptime(old_update_date, db_date_format)    
+            print ("Document ", id, " update date ", old_update_date, " will change to ", new_update_date, 
+                flush=True)
+        except:
+            print ("Document ", id, " update date", old_update_date, "does not match the format provided" ,
+                flush=True)
+    else:
+        print ("Document ", id," does not have the update field ", update_date_field, " and will be set to ", 
+            new_update_date, flush=True)
+
+
+def updateDocument(doc, targetCollections):
+    id = doc["_id"]
+
+    #we only want to do an update if either date needs its format changed
+    has_update = False
+
+    # the "added to database" field may or may not exist
+    #  if it does not, make it null
+    try:
+        old_insert_date = doc[insert_date_field]
+    except:
+        old_insert_date = None
+
+    try:
+        old_update_date = doc[update_date_field]
+    except:
+        old_update_date = None
+
     # try to convert old insert date to new format. If it doesn't exist, or is null, keep it so
     #   otherwise, if conversion with supplied mask can be applied, convert it, otherwise, leave it unchanged
     if (old_insert_date is None):
@@ -46,18 +110,43 @@ def updateDocument(doc, targetCollections):
         try:
             old_insert_datetime = datetime.datetime.strptime(old_insert_date, db_date_format)
             new_insert_date = old_insert_datetime.isoformat()
+            has_update = True
         except:
             new_insert_date = old_insert_date
 
-    new_update_date = datetime.datetime.now().isoformat()
+    #we want to change update date by default if it's not set (that's the update), or
+    #  if the insert date has been updated
+    if ((old_update_date is None) or (has_update is True)):
+        new_update_date = datetime.datetime.now().isoformat()
+        has_update = True
+    else:
+        # if old update date has a mask and we haven't changed insert date, update it still
+        try: 
+            old_update_datetime = datetime.datetime.strptime(old_update_date, db_date_format)
+            new_update_date = datetime.datetime.now().isoformat()
+            has_update = True
+        except:
+            new_update_date = old_update_date
 
-    db_cm.update({"_id": id},{"$set":{insert_date_field: new_insert_date, update_date_field: new_update_date}})
+    if (has_update):
+        db_cm.update({"_id": id},{"$set":{insert_date_field: new_insert_date, update_date_field: new_update_date}})
 
+# we want to iterate all the entries in a collection, except if
+#  we are checking sequences for valid date format - because it would 
+#  take too long, pick one, because date format should be consistent accross
+#  the repository
 record_count = 0
-record_list = db_cm.find();
+if (no_update and collection_name in rearrangement_names):
+    record_list = db_cm.find().limit(1)
+else: 
+    record_list = db_cm.find()
+print ("Starting the proces at ",datetime.datetime.now().isoformat(), flush=True )
 for r in record_list:
     record_count +=1
-    updateDocument(r,db_cm)
+    if (no_update):
+        check_document(r)
+    else:
+        updateDocument(r,db_cm)
     if (record_count % 1000000 == 0):
-        print ("Processing record ", record_count)
-print (datetime.datetime.now().isoformat())
+        print ("Processing record ", record_count, flush=True)
+print ("Ended the process at ", datetime.datetime.now().isoformat(), flush=True)
--- dataload/update_keywords_study.py
+++ dataload/update_keywords_study.py
@@ -4,6 +4,7 @@ import pandas as pd
 import pymongo
 import json
 import sys
+import datetime
 
 
 #script to update keywords_study field to use ADC v 1.4 terms
@@ -28,6 +29,7 @@ db_cm = mng_db[sample_collection_name]
 keywords_study_field = sys.argv[4]
 single_cell_field = sys.argv[5]
 sequence_count_field = sys.argv[6]
+update_date_field = sys.argv[7]
 
 
 keyword_translation = { "contains_ig": "contains_ig", 
@@ -47,33 +49,45 @@ def updateDocument(doc, targetCollections):
 
     updated_keywords_study = list()
 
+    has_update = False
     old_keywords_study = list()
     try:
         old_keywords_study_value = doc[keywords_study_field]
         # some older repositories have keywords_study as a string, we want it as array
         if type(old_keywords_study_value) == str:
-            old_keywords_study.append(old_keywords_study_value)            
+            old_keywords_study.append(old_keywords_study_value)    
+            has_update = True         
         else:
             old_keywords_study.extend(old_keywords_study_value)
     except: 
-        print ("For sample _id ", id, " could not find study_keywords.\n")
+        print ("For sample _id ", id, " could not find study_keywords.", flush=True)
 
     for keyword in old_keywords_study:
         if keyword in keyword_translation:
             updated_keywords_study.append(keyword_translation[keyword])
-
+            if (keyword_translation[keyword] != keyword):
+                has_update = True
+ 
     if sequence_count > 0 and "contains_schema_rearrangement" not in updated_keywords_study: 
         updated_keywords_study.append("contains_schema_rearrangement")
+        has_update = True
 
     if has_single_cell and "contains_paired_chain" not in updated_keywords_study:
         updated_keywords_study.append("contains_paired_chain")
+        has_update = True
 
-    db_cm.update({"_id": id},{"$set":{keywords_study_field: updated_keywords_study}})
+    if (has_update):
+        new_update_date = datetime.datetime.now().isoformat()
+        print ("Updating ", id, " with ", updated_keywords_study, flush=True)
+        db_cm.update({"_id": id},{"$set":{keywords_study_field: updated_keywords_study, 
+            update_date_field: new_update_date}})
 
 record_count = 0
 record_list = db_cm.find();
+print ("Starting the proces at ",datetime.datetime.now().isoformat(), flush=True )
 for r in record_list:
     record_count +=1
     updateDocument(r,db_cm)
-    if (record_count % 1000000 == 0):
-        print ("Processing record ", record_count)
+    if (record_count % 100 == 0):
+        print ("Processing record ", record_count, flush=True)
+print ("Ended the process at ", datetime.datetime.now().isoformat(), flush=True)
