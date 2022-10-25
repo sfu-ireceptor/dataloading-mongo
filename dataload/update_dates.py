eb43cbb8902060c0065f5d2ab34959875e3d32ed
bzimonja <30536574+bzimonja@users.noreply.github.com>
2022-10-25 09:24:22 -0700

    -add update scripts for template amount and timepoint, improvements to all scripts
    -add the script to update template amount from string to amount/units pair
    -add the script to adjust timepoint_relative from string to number/ontology id/ontology value triplet
    -add check/check-verbose/verbose parameters
    -add more details to output in verbose mode
    -add return value of 0 or 1, meaning of value depends on if the script is run in check or update mode

--- dataload/update_dates.py
+++ dataload/update_dates.py
@@ -26,17 +26,34 @@ insert_date_field = sys.argv[4]
 update_date_field = sys.argv[5]
 db_date_format = sys.argv[6]
 
-# optional 'noupdate' argument if user wants to test what an update would do
+# optional noupdate argument if user wants to test what an update would do
 #   without affecting the database
+# used values are "check", "verbose" and "check-verbose"
 try: 
-    no_update = sys.argv[7]
-except:
-    no_update = False
+    noupdate = sys.argv[7]
+    if (noupdate not in ["check", "verbose", "check-verbose"]):
+        print ("Valid parameters are check, verbose and check-verbose")
+        exit(0)
+except IndexError:
+    noupdate = False
 
-if (no_update == "noupdate"):
-    no_update = True 
-else:
-    no_update = False
+# set default arguments for 'check' and 'verbose' switches, and
+#  set a global variable that will show if update is possible or not
+#  so script can inform the user
+verbose = False
+update = True
+possible_update = False
+had_warnings = False
+
+if (noupdate == 'check'):
+    update = False
+
+if (noupdate == 'verbose'):
+    verbose = True
+
+if (noupdate == 'check-verbose'):
+    update = False
+    verbose = True
 
 # list of names that are used for rearrangement collection
 rearrangement_names = ["sequence", "sequences", "rearrangement", "rearrangements"]
@@ -46,6 +63,8 @@ rearrangement_names = ["sequence", "sequences", "rearrangement", "rearrangements
 def check_document(doc):
     id = doc["_id"]
 
+    global possible_update
+    global had_warnings
     # the "added to database" field may or may not exist
     #  if it does not, make it null
     try:
@@ -62,30 +81,42 @@ def check_document(doc):
         try:
             old_insert_datetime = datetime.datetime.strptime(old_insert_date, db_date_format)
             new_insert_date = old_insert_datetime.isoformat()
-            print ("Document ", id, " insert date ", old_insert_date, " will change to ", new_insert_date, 
-                flush=True)
+            possible_update = True
+            if (verbose == True):
+                print ("Document ", id, "insert date", old_insert_date, "will change to", new_insert_date, 
+                    flush=True)
         except:
-            print ("Document ", id," insert date ",old_insert_date ," does not match the format provided",
-                flush=True)
+            had_warnings = True
+            if (verbose == True):
+                print ("Document", id,"insert date",old_insert_date ,"does not match the format provided",
+                    flush=True)
     else:
-        print ("Document ", id," does not have the insert field ", insert_date_field, flush=True)
+        had_warnings = True
+        if (verbose == True):
+            print ("Document", id,"does not have the insert field", insert_date_field, flush=True)
 
     new_update_date = datetime.datetime.now().isoformat()
     if (old_update_date is not None):
         try:
-            old_update_datetime = datetime.datetime.strptime(old_update_date, db_date_format)    
-            print ("Document ", id, " update date ", old_update_date, " will change to ", new_update_date, 
-                flush=True)
+            old_update_datetime = datetime.datetime.strptime(old_update_date, db_date_format) 
+            possible_update = True
+            if (verbose == True):   
+                print ("Document", id, "update date", old_update_date, "will change to ", new_update_date, 
+                    flush=True)
         except:
-            print ("Document ", id, " update date", old_update_date, "does not match the format provided" ,
-                flush=True)
+            had_warnings = True
+            if (verbose == True):            
+                print ("Document ", id, "update date", old_update_date, "does not match the format provided" ,
+                    flush=True)
     else:
-        print ("Document ", id," does not have the update field ", update_date_field, " and will be set to ", 
-            new_update_date, flush=True)
-
+        had_warnings = True
+        if (verbose == True):
+            print ("Document ", id,"does not have the update field", update_date_field, flush=True)
 
 def updateDocument(doc, targetCollections):
     id = doc["_id"]
+    global possible_update
+    global had_warnings
 
     #we only want to do an update if either date needs its format changed
     has_update = False
@@ -96,6 +127,8 @@ def updateDocument(doc, targetCollections):
         old_insert_date = doc[insert_date_field]
     except:
         old_insert_date = None
+        if (verbose == True):
+            print ("Document", id,"does not have the insert field ", insert_date_field, flush=True)
 
     try:
         old_update_date = doc[update_date_field]
@@ -112,13 +145,15 @@ def updateDocument(doc, targetCollections):
             new_insert_date = old_insert_datetime.isoformat()
             has_update = True
         except:
+            had_warnings = True
             new_insert_date = old_insert_date
+            if (verbose == True):            
+                print ("Document", id, "update date", old_update_date, "does not match the format provided" ,
+                    flush=True)
 
-    #we want to change update date by default if it's not set (that's the update), or
-    #  if the insert date has been updated
-    if ((old_update_date is None) or (has_update is True)):
+    #we want to change update date only if the insert date has been updated
+    if (has_update == True):
         new_update_date = datetime.datetime.now().isoformat()
-        has_update = True
     else:
         # if old update date has a mask and we haven't changed insert date, update it still
         try: 
@@ -126,27 +161,56 @@ def updateDocument(doc, targetCollections):
             new_update_date = datetime.datetime.now().isoformat()
             has_update = True
         except:
+            if (verbose == True):            
+                print ("Document ", id, "update date", old_update_date, "does not match the format provided" ,
+                    flush=True)
             new_update_date = old_update_date
 
     if (has_update):
-        db_cm.update({"_id": id},{"$set":{insert_date_field: new_insert_date, update_date_field: new_update_date}})
+        possible_update = True
+        if (verbose == True):
+            print ("Setting new values for _id", id, "insert date", new_insert_date, 
+                "update_date", new_update_date, flush=True)
+        db_cm.update({"_id": id},{"$set":{insert_date_field: new_insert_date, 
+            update_date_field: new_update_date}})
 
 # we want to iterate all the entries in a collection, except if
 #  we are checking sequences for valid date format - because it would 
 #  take too long, pick one, because date format should be consistent accross
 #  the repository
 record_count = 0
-if (no_update and collection_name in rearrangement_names):
+if (update == False and collection_name in rearrangement_names):
     record_list = db_cm.find().limit(1)
 else: 
     record_list = db_cm.find()
 print ("Starting the proces at ",datetime.datetime.now().isoformat(), flush=True )
 for r in record_list:
     record_count +=1
-    if (no_update):
+    if (update == False):
         check_document(r)
     else:
         updateDocument(r,db_cm)
-    if (record_count % 1000000 == 0):
+    if (record_count % 1000000 == 0 and verbose == True):
         print ("Processing record ", record_count, flush=True)
 print ("Ended the process at ", datetime.datetime.now().isoformat(), flush=True)
+#print out any warnings in verbose mode
+if (had_warnings == True):
+    print ("There were issues with some of the attempted updates", flush=True)
+    if (verbose == True):
+        print ("Please consult the program output for warnings")
+    else: 
+        print ("Consider running the script with verbose or check-verbose option")
+
+#if we were in check mode and there was a possible update, return 1, otherwise 0
+#else, return 1 if something went wrong, and 0 if not
+if (update == False):
+    if (possible_update == True):
+        print (1, flush=True)
+    else:
+        print (0, flush=True)
+else: 
+    if (had_warnings == True):
+        print (1, flush=True)
+    else:
+        print (0, flush=True)
+
--- dataload/update_keywords_study.py
+++ dataload/update_keywords_study.py
@@ -31,6 +31,33 @@ single_cell_field = sys.argv[5]
 sequence_count_field = sys.argv[6]
 update_date_field = sys.argv[7]
 
+# optional noupdate argument if user wants to test what an update would do
+#   without affecting the database
+# used values are "check", "verbose" and "check-verbose"
+try: 
+    noupdate = sys.argv[8]
+    if (noupdate not in ["check", "verbose", "check-verbose"]):
+        print ("Valid parameters are check, verbose and check-verbose")
+        exit(0)
+except IndexError:
+    noupdate = False
+
+# set default arguments for 'check' and 'verbose' switches, and
+#  set a global variable that will show if update is possible or not
+#  so script can inform the user
+verbose = False
+update = True
+possible_update = False
+had_warnings = False
+if (noupdate == 'check'):
+    update = False
+
+if (noupdate == 'verbose'):
+    verbose = True
+
+if (noupdate == 'check-verbose'):
+    update = False
+    verbose = True
 
 keyword_translation = { "contains_ig": "contains_ig", 
     "contains_tcr": "contains_tr", 
@@ -47,9 +74,11 @@ def updateDocument(doc, targetCollections):
     sequence_count = doc[sequence_count_field]
     has_single_cell = doc[single_cell_field]
 
-    updated_keywords_study = list()
-
+    global possible_update
+    global had_warnings
     has_update = False
+
+    updated_keywords_study = list()
     old_keywords_study = list()
     try:
         old_keywords_study_value = doc[keywords_study_field]
@@ -60,27 +89,45 @@ def updateDocument(doc, targetCollections):
         else:
             old_keywords_study.extend(old_keywords_study_value)
     except: 
-        print ("For sample _id ", id, " could not find study_keywords.", flush=True)
-
+        had_warnings = True
+        if (verbose == True):
+            print ("For sample _id", id, "could not find study_keywords.", flush=True)
+        return()
+            
+    #check if keyword exists in translation table and pass it through
+    #  if it doesn't,  throw a warning
+    # we have a case where contains_paired_chain and contains_single cell could be both
+    #  in the old values, so check that the new value is not already in the update array
     for keyword in old_keywords_study:
-        if keyword in keyword_translation:
-            updated_keywords_study.append(keyword_translation[keyword])
+        if (keyword in keyword_translation):
+            if (keyword_translation[keyword] not in updated_keywords_study):
+                updated_keywords_study.append(keyword_translation[keyword])
             if (keyword_translation[keyword] != keyword):
                 has_update = True
+        else: 
+            if (verbose == True):
+                print ("For sample _id", id, "could not identiy keyword ", keyword, flush=True)
+            had_warnings = True
+            return()
  
-    if sequence_count > 0 and "contains_schema_rearrangement" not in updated_keywords_study: 
+    if (sequence_count > 0 and "contains_schema_rearrangement" not in updated_keywords_study): 
         updated_keywords_study.append("contains_schema_rearrangement")
         has_update = True
 
-    if has_single_cell and "contains_paired_chain" not in updated_keywords_study:
+    if (has_single_cell and "contains_paired_chain" not in updated_keywords_study):
         updated_keywords_study.append("contains_paired_chain")
         has_update = True
 
     if (has_update):
+        possible_update = True
         new_update_date = datetime.datetime.now().isoformat()
-        print ("Updating ", id, " with ", updated_keywords_study, flush=True)
-        db_cm.update({"_id": id},{"$set":{keywords_study_field: updated_keywords_study, 
-            update_date_field: new_update_date}})
+        if (verbose == True):
+            print ("Updating", id, "with", updated_keywords_study, flush=True)
+        if (update == True):
+            db_cm.update({"_id": id},{"$set":{keywords_study_field: updated_keywords_study, 
+                update_date_field: new_update_date}})
+    else: 
+        print ("For _id", id, "there is no need to update the keywords", old_keywords_study, flush=True)
 
 record_count = 0
 record_list = db_cm.find();
@@ -88,6 +135,26 @@ print ("Starting the proces at ",datetime.datetime.now().isoformat(), flush=True
 for r in record_list:
     record_count +=1
     updateDocument(r,db_cm)
-    if (record_count % 100 == 0):
+    if (record_count % 100 == 0 and verbose == True):
         print ("Processing record ", record_count, flush=True)
 print ("Ended the process at ", datetime.datetime.now().isoformat(), flush=True)
+#print out any warnings in verbose mode
+if (had_warnings == True):
+    print ("There were issues with some of the attempted updates", flush=True)
+    if (verbose == True):
+        print ("Please consult the program output for warnings")
+    else: 
+        print ("Consider running the script with verbose or check-verbose option")
+
+#if we were in check mode and there was a possible update, return 1, otherwise 0
+#else, return 1 if something went wrong, and 0 if not
+if (update == False):
+    if (possible_update == True):
+        print (1, flush=True)
+    else:
+        print (0, flush=True)
+else: 
+    if (had_warnings == True):
+        print (1, flush=True)
+    else:
+        print (0, flush=True)
